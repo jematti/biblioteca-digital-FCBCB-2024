39a688029f2881a4f7d7026df15821ce24e6d0a9
jematti <jokemz1996@gmail.com>
2022-11-24 17:03:32 -0400

    correcion reportes

--- resources/views/orders/sale.blade.php
+++ resources/views/orders/sale.blade.php
@@ -4,17 +4,8 @@
 
 <div class="max-w-7xl mx-auto ">
     <h2 class=" my-5  bg-custom-100 text-white uppercase text-lg rounded-lg p-4 text-center font-bold ">Reporte de Ventas Realizadas</h2>
-    {{-- mensaje de alerta --}}
-    {{-- @if (session('info'))
-    <div id="alert-2" class="flex p-4 mb-4 bg-red-100 rounded-lg dark:bg-red-200" role="alert">
-        <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-red-700 dark:text-red-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
-        <span class="sr-only">Info</span>
-        <div class="ml-3 text-base font-medium text-red-700 dark:text-red-800">
-            {{ session('info') }}
-        </div>
-      </div>
-    @endif --}}
-    <form  action="{{ route('sale.pdf') }}" method="POST" novalidate >
+
+    <form  action="{{ route('sale.pdf') }}" method="POST" novalidate target="_blank">
         @csrf
         <div class="md:grid md:grid-cols-2 gap-5">
             <div class="mb-5">
@@ -79,10 +70,8 @@
         </div>
 
         <div class="flex justify-end">
-            <input type="submit" class="bg-indigo-500 hover:bg-indigo-600 transition-colors text-white text-sm font-bold px-10 py-2 rounded cursor-pointer uppercase w-1/2" value="Generar Reporte PDF" />
+            <input type="submit" class="bg-indigo-500 hover:bg-indigo-600 transition-colors text-white text-sm font-bold px-10 py-2 rounded cursor-pointer uppercase w-1/2" value="Generar Reporte PDF" target="_blank"/>
         </div>
     </form>
-
-
 </div>
 @endsection
