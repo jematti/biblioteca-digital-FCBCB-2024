eaf139c162a9ba1c810087c675cecf5ded816883
jematti <jokemz1996@gmail.com>
2022-12-08 11:12:20 -0400

    correcion añadir item

--- resources/views/livewire/add-cart-item.blade.php
+++ resources/views/livewire/add-cart-item.blade.php
@@ -5,7 +5,7 @@
         <button
         disabled
         x-bind:disabled="$wire.cantidad <= 1"
-        wire:loading.attr="dissabled"
+        wire:loading.attr="disabled"
         wire:target="decrement"
         wire:click="decrement"
         class="text-black font-bold border-2 border-black bg-white px-3 py-1 rounded hover:bg-indigo-900 hover:text-white"
@@ -13,7 +13,7 @@
         <span class="m-5">{{ $cantidad }}</span>
         <button
         x-bind:disabled="$wire.cantidad >= $wire.cantidad_stock"
-        wire:loading.attr="dissabled"
+        wire:loading.attr="disabled"
         wire:target="increment"
         wire:click="increment"
         class="text-black font-bold border-2 border-black bg-white px-3 py-1 rounded hover:bg-indigo-900 hover:text-white"
